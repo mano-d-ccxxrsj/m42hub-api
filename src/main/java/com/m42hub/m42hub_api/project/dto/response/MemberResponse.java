80d8b1ba3d038c295590162a5691621fdac030d6
betgrave <betgrave@gmail.com>
2025-08-27 20:53:24 -0300

    fix: roleId on member DTO

--- src/main/java/com/m42hub/m42hub_api/project/dto/response/MemberResponse.java
+++ src/main/java/com/m42hub/m42hub_api/project/dto/response/MemberResponse.java
@@ -10,8 +10,8 @@ import java.time.LocalDateTime;
 public record MemberResponse(
         Long id,
         Boolean isManager,
-        Long project,
-        Long role,
+        Long projectId,
+        Long roleId,
         AuthenticatedUserResponse user,
         MemberStatusResponse memberStatus,
         String applicationMessage,
